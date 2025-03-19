4e31eba4aec6fe7c722ae465f4f86986cb8be2fd
tahirtpd <gm24543@bristol.ac.uk>
2025-03-19 17:06:37 +0000

    Mr Holmes Pro Max 1TB

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -1,10 +1,8 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
-<<<<<<< Updated upstream
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
@@ -16,16 +14,10 @@ import io.atlassian.fugue.Pair;
 import uk.ac.bris.cs.scotlandyard.model.Ai;
 import uk.ac.bris.cs.scotlandyard.model.Board;
 import uk.ac.bris.cs.scotlandyard.model.Move;
-=======
 import java.util.*;
-import java.util.concurrent.TimeUnit;
-import javax.annotation.Nonnull;
-
 import com.google.common.collect.ImmutableSet;
 import com.google.common.graph.*;
-import io.atlassian.fugue.Pair;
 import uk.ac.bris.cs.scotlandyard.model.*;
->>>>>>> Stashed changes
 
 public class MyAi implements Ai {
 
@@ -34,37 +26,31 @@ public class MyAi implements Ai {
 	@Nonnull @Override public Move pickMove(
 			@Nonnull Board board,
 			Pair<Long, TimeUnit> timeoutPair) {
-<<<<<<< Updated upstream
 		// returns a random move, replace with your own implementation
-		// Greedy algorithm 
+		// Greedy algorithm
 
 		/*
 		 * Move that increases the moves required by all detectives (moves count now quantified)
 		*/
-=======
 		// Greedy algorithm
 
 		/*
 		 * Move that increases the moves required by all detectives (moves count now quantified)
 		 */
->>>>>>> Stashed changes
 
 		// assume detectives will play a move that approaches mrx (hold all positions that put det equally close to mrx)
 		// assume detectives && mrx have unlimited tickets
 		// find new locaiton of all dets
 		// parallel
 		// for each detective:
-<<<<<<< Updated upstream
 			// do dijkstra's on all nodes on graph.
 			// second pass: go over each node in new graph (node: distance)
 			// // multiply distance with detective weight for new "score"
 			// pass the distance itself into fn: e^d (optional: for each node connecting to itself, sum the connections of these nodes and add to total score)
-=======
 		// do dijkstra's on all nodes on graph.
 		// second pass: go over each node in new graph (node: distance)
 		// // multiply distance with detective weight for new "score"
 		// pass the distance itself into fn: e^d (optional: for each node connecting to itself, sum the connections of these nodes and add to total score)
->>>>>>> Stashed changes
 
 		// for each graph:
 		// for each of identical nodes:
@@ -74,8 +60,6 @@ public class MyAi implements Ai {
 		// find the score in the new single graph
 		// find move with max score
 		// return move
-<<<<<<< Updated upstream
-=======
 
 		// let mut graphs = vec![];
 		// for (Piece plr : board.getPlayers()) {
@@ -85,37 +69,10 @@ public class MyAi implements Ai {
 		// 		graphs.push(graph);
 		// 	}
 		// } */
-		Dijkstras(board.getDetectiveLocation(null).get(), board.getSetup().graph.asGraph());
->>>>>>> Stashed changes
 
-		// let mut graphs = vec![];
-		// for (Piece plr : board.getPlayers()) {
-		// 	if (plr.isDetective()) {
-		// 		let graph = Dijkstras(board.getDetectiveLocation((Detective) plr).get(), board.getSetup().graph);
-		// 		let graph = score(graph);
-		// 		graphs.push(graph);
-		// 	}
-		// }
+
 		Dijkstras(board.getDetectiveLocation(null).get(), board.getSetup().graph.asGraph());
 
-<<<<<<< Updated upstream
-		// let mut new_graph = board.getSetup().graph;
-		// for node in new_graph {
-		// 	let mut total_score = 0;
-		// 	for graph in graphs {
-		// 		total_score += graph.get(node);
-		// 	}
-		// }
-
-		// let mut best = null;
-		// for move in mrx moves {
-		// 	if new_graph.get(move).score > best.score {
-		// 		best = move
-		// 	}
-		// }
-
-		// return best
-=======
 		List<Move> moves = board.getAvailableMoves().asList();
 		ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> new_graph = board.getSetup().graph;
 		ImmutableSet<Piece> players = board.getPlayers();
@@ -154,7 +111,6 @@ public class MyAi implements Ai {
 
 		int bestScore = 0;
 		Move bestMove = null;
->>>>>>> Stashed changes
 
 		MutableValueGraph<Integer, Integer> b = ValueGraphBuilder.undirected().build();
 		for (Integer node : new_graph.nodes()) {
@@ -180,8 +136,7 @@ public class MyAi implements Ai {
 				bestScore = score.get();
 			}
 		}
-
-
+		
 		return bestMove;
 	}
 
@@ -241,76 +196,6 @@ public class MyAi implements Ai {
 		// could do page rank
 		// return graph
 		return null;
-	}
- 
-	/** 
-	 * <p>assume distance between nodes are never Integer.MAX_VALUE</p> 
-	 * @return <b>Node : Distance</b>
-	*/
-	private ImmutableValueGraph<Integer, Integer> Dijkstras(Integer detectiveLocation, ImmutableGraph<Integer> graph) {
-		List<Node> visited = new ArrayList<>();
-		Set<Integer> visitedLocation = new HashSet<>();
-
-		visited.add(new Node(null, detectiveLocation, 0));
-		visitedLocation.add(detectiveLocation);
-
-		Integer minimumFrom = null;
-		Integer minimumNode = null;
-		Integer minimumDistance = null;
-
-		while (minimumDistance != Integer.MAX_VALUE) {
-			minimumDistance = Integer.MAX_VALUE;
-
-			for (Node visitedNode : visited) {
-				for (Integer connection : graph.adjacentNodes(visitedNode.location)) {
-					if (visitedLocation.contains(connection)) {
-						continue;
-					}
-					
-					// cost of edge is 1
-					if (1 + visitedNode.distance < minimumDistance) {
-						minimumDistance = 1 + visitedNode.distance;
-						minimumFrom = visitedNode.location;
-						minimumNode = connection;
-					}
-				}
-			}
-
-			visited.add(new Node(minimumFrom, minimumNode, minimumDistance));
-			visitedLocation.add(minimumNode);
-		}
-
-		visited.remove(visited.size() - 1);
-
-		MutableValueGraph<Integer, Integer> newGraph = ValueGraphBuilder.undirected().build();
-
-		for (Node n : visited) {
-			newGraph.addNode(n.from);
-			newGraph.addNode(n.location);
-			newGraph.putEdgeValue(n.from, n.location, n.distance);
-		}
-
-		return ImmutableValueGraph.copyOf(newGraph);
-	}
-
-	private ImmutableValueGraph<Integer, Integer> Score(ImmutableValueGraph<Integer, Integer> graph) {
-		// go through graph
-		// apply fn exagerate(distance) -> score; 
-		// could do page rank
-		// return graph
-		return null;
-	}
-}
-
-class Node {
-	Integer from;
-	Integer location;
-	Integer distance;
-
-	Node(Integer from, Integer location, Integer distance) {
-		this.from = from;
-		this.location = location;
-		this.distance = distance;
 	}
 }
 
